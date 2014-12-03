0347ec575dada2f018c8c61ae3b40f70ae6606c1
Kanda <liannayangse@gmail.com>
2014-12-02 23:09:21 -0500

    error message code

--- Purchase.py
+++ Purchase.py
@@ -3,9 +3,27 @@
 
 # enable debugging
 import cgitb
-cgitb.enable()
 
-print "Content-Type: text/plain;charset=utf-8"
-print
+def generateBill():
+	
 
-print "Hello World!"
+def generateError():
+	import webbrowser
+
+	f = open('error.html','w')
+
+	message = """<html>
+	<head></head>
+	<body>
+	<p>Since you are not logged in, your order could not be processed.</p>
+	<p>Please log in <a href = login.html>here</a>.</p>
+	</body>
+	</html>"""
+
+	f.write(message)
+	f.close()
+
+	webbrowser.open_new_tab('helloworld.html')
+	
+if username == "":
+	generateError()
