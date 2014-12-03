616715d6e329e7c131859045eaafb7f648e9a5ef
Lianna Yang <lianna.yang@mail.mcgill.ca>
2014-12-03 16:57:03 -0500

    Generates error page

---
+++ cgi-bin/Purchase.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+# enable debugging	
+import webbrowser
+import cgitb
+cgitb.enable()
+
+print "Content-Type: text/plain;charset=utf-8"
+print
+
+def generateError():
+	f = open('error.html','w')
+
+	message = """<html>
+	<head></head>
+	<body>
+	<p>Since you are not logged in, your order could not be processed.</p>
+	<p>Please log in by clicking <a href = login.html>here</a>.</p>
+	</body>
+	</html>"""
+
+	f.write(message)
+	f.close()
+
+	webbrowser.open('error.html', new=2, autoraise=True)
+
+generateError()	
