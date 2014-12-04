3ec53634fa22ac85788da29af2500ff4b0b80b24
Lianna Yang <lianna.yang@mail.mcgill.ca>
2014-12-04 16:19:53 -0500

    Got error page generation to work, changed layout in catalogue

--- cgi-bin/Purchase.py
+++ cgi-bin/Purchase.py
@@ -1,29 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
-# enable debugging	
-import webbrowser
+import cgi
 import cgitb
 cgitb.enable()
 
-print "Content-Type: text/plain;charset=utf-8"
+print "Content-Type: text/html;charset=utf-8"
 print
 
-def generateError():
-	f = open('error.html','w')
+def generateError(): 
 
-	message = """<html>
-	<head></head>
-	<body>
-	<p>Since you are not logged in, your order could not be processed.</p>
-	<p>Please log in by clicking <a href = ../login.html>here</a>.</p>
-	</body>
-	</html>"""
+	print """
+<!doctype html>
+<html>
+<head></head>
+<body>
+<p>Since you are not logged in, your order could not be processed.</p>
+<p>Please log in by clicking <a href = ../login.html>here</a>.</p>
+</body>
+</html>"""
 
-	f.write(message)
-	f.close()
-	url = " cgi-bin/error.html"	
-	webbrowser.open(url)
 
-webbrowser.open('www.google.com')
-generateError()	
+generateError()
