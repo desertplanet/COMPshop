9465c672474b191da1f50771aa9d537473fe408b
Lianna Yang <lianna.yang@mail.mcgill.ca>
2014-12-04 21:25:11 -0500

    added new method to check stock in inventory

--- cgi-bin/Purchase.py
+++ cgi-bin/Purchase.py
@@ -13,7 +13,7 @@ def generateError():
 	<!doctype html>
 	<html>
 	<head>
-	<title>Error</title>
+	<title>Not Logged In</title>
 	<style type="text/css"></style>
 	</head>
 	<body style="color:black; font-family:helvetica;">
@@ -27,7 +27,7 @@ def generateError():
 	</body>
 	</html>"""
 
-def generateBill():
+def generateBill(n):
 	print "Content-Type: text/html;charset=utf-8"
 	print
 	print """
@@ -45,49 +45,43 @@ def generateBill():
 	<body>"""
 	mylist = ["retriever", "labrador", "pug", "pitbull"]
 	total = 0
-	print """<table>"""	
+	print """<table>"""
+	print """<thead>""" + getStock(mylist[0]) + """</thead."""	
 	with open('../Inventory.csv', 'rt') as csvfile:
 		spamreader = csv.reader(csvfile, delimiter=',', quotechar=' ')
 		for row in spamreader:
-			'''if gotPuppy(row[0], mylist) = True:
-				print """<tr><td>""" + true + """</td></tr>"""'''
-			print """<tr><td>""" + mylist[0] + """</td><td>x""" + str(row[1]) + """</td><td>$""" + str(row[2]) + """</td></tr>"""
-			total = total + int(row[1])*int(row[2])
+			print """<tr><td>""" + mylist[0] + """</td><td>x""" + n + """</td><td>$""" + str(row[2]) + """</td></tr>"""
+			total = total + int(n)*int(row[2])
 	print """<tr><td>	</td><td>Total</td><td>$""" + str(total) + """</td></tr>"""
 	print """</table>
 	</body>
 	</html>"""
 
+def getStock(pup):
+	with open('../Inventory.csv', 'rt') as cfile:
+		sreader = csv.reader(cfile, delimiter=',', quotechar=' ')
+		for m in sreader:
+			return m[1]
+
+form = cgi.FieldStorage()
+
+liuser = form.getfirst("username")
+ofeach = form.getfirst("num")
+ulength = len(liuser)
+
+if int(ulength) > 0:
+	generateBill(ofeach)
 
 
-"""def gotPuppy(str, list):
-	yn = False	
-	for var in list:
-		if var = str:
-			yn = True
-	return yn"""
 
 
-"""form = cgi.FieldStorage()
-
-liuser = form.getfirst("username").upper()
-
-
-with open('../Members.csv', 'rt') as csvfile:
+"""with open('../Members.csv', 'rt') as csvfile:
 	spamreader = csv.reader(csvfile, delimiter=',', quotechar=' ')
 	print "Content-Type: text/plain;charset=utf-8"
 	print	
 	for row in spamreader:
 		print row[0])"""
 
-
-'''txt = open("../LoggedIn.csv", 'r')
-
-print "Content-Type: text/plain;charset=utf-8"
-print
-print txt.read()'''
-
-generateBill()
 
 
 
