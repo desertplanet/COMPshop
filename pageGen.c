0b2f5a0a5f20b99f7bd961f0c3a7c6a5b2cd153d
Dune DESORMEAUX <dune.desormeaux@gmail.com>
2014-12-06 00:23:07 -0500

    IT VURKS

--- pageGen.c
+++ pageGen.c
@@ -1,41 +1,45 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
-int main (int argc, char *argv[]) {
+int main (void) {
 
 	
-		char syscmd[50];
+		char *syscmd = (char *)calloc(50,1);
 
-		char input[40];
+		char *input = (char *)calloc(40,1);
 
-		char uName[30];
+		char *uName = (char *)calloc(40,1);
+
 
 		fgets(input,40,stdin);
 
-		int i = 9;
+		int i = 0;
 
-		// while (input[i] != '&') {
-		// 	uName[i] = input[i];
-		// }
+		while (*(i+input) != '\0') {
+			*(i+uName) = *(9+i+input);
+			i++;
+		}
 
-		scanf(syscmd,"python createCatalogue.py %s", uName);
+		sprintf(syscmd,"python cgi-bin/createCatalogue.py %s",uName);
 
-		printf("content-type: text/html");
+		printf("content-type: text/html\n\n");
 
 		printf("%s\n", syscmd);
+
 		
-		// system(syscmd);
+		system(syscmd);
 
 	
-		// FILE *f = fopen("catalogue.html","rt");
+		FILE *f = fopen("catalogue.html","rt");
 
-		// char line[120];
+		char line[120];
 
 		
 
-		// while (fgets(line,120,f) != NULL) {
-		// 	printf("%s", line);
-		// }
+		while (fgets(line,120,f) != NULL) {
+			printf("%s", line);
+		}
 
-	return(0);
+	return 0;
 }
