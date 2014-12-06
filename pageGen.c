e7f8730a5c8767bf7fad786a3f88b8773f5b9d3b
Dune DESORMEAUX <dune.desormeaux@gmail.com>
2014-12-05 22:41:17 -0500

    generic file generation cgi script.

---
+++ pageGen.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main (int argc, char *argv[]) {
+
+	if (argc == 2) {
+		FILE *f = fopen(argv[1],"rt");
+
+		char line[120];
+
+		while (fgets(line,120,f) != NULL) {
+			printf("%s", line);
+		}
+
+	} else {
+		printf("FILE ERROR\n");
+		return(1);
+	}
+
+	return(0);
+}
