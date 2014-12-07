7b539cb8df06b6a078b9cb78c3b9e8cda0711964
Lianna Yang <lianna.yang@mail.mcgill.ca>
2014-12-07 04:47:38 -0500

    version with split and join instead of csv reader because for some unfathomable reason, we can't use the csv library, which is standard to python, to read a damned csv file

--- cgi-bin/Purchase.py
+++ cgi-bin/Purchase.py
@@ -17,25 +17,18 @@ def generateError():
 	<!doctype html>
 	<html>
 	<head>
-	<link rel="stylesheet" type="text/css" href="../stylesheets/main.css">
 	<title>Not Logged In</title>
 	<style type="text/css"></style>
 	</head>
 	<body style="color:black; font-family:helvetica;">
-	<div id = "home">
-	<center>
+	<div style="float:left; width:100%;">
+	<div style="text-align:center;
+	margin:0 auto;
+	width:40%;">
 	<body>
 	<p>Since you are not logged in, your order could not be processed.</p>
-	<form name="goback2home" method="post" action="../index.html">
-	<input name = "username" type = "hidden" value ="null"></input>
-	<input type = "submit" value = "home"></input>
-	</form>
-	<form name="goback" method="post" action="../c.cgi">
-	<input name = "username" type = "hidden" value ="null"></input>
-	<input type = "submit" value = "catalogue"></input>
-	</form>
-	</center>
-	</div>
+	<p>Please log in by clicking <a href = ../login.html>here</a>.</p>
+	<p>To return to the catalogue, click <a href = ../catalogue.html>here</a>.</p>
 	</body>
 	</html>"""
 
@@ -54,13 +47,15 @@ def generateBill(username, n):
 	<!doctype html>
 	<html>
 	<head>
-	<link rel="stylesheet" type="text/css" href="../stylesheets/main.css">
 	<title>Bill</title>
 	<style type="text/css"></style>
 	</head>
-	<div id="home">
-	<body>
-	<center>"""
+	<body style="color:black; font-family:helvetica;">
+	<div style="float:left; width:100%;">
+	<div style="text-align:center;
+	margin:0 auto;
+	width:40%;">
+	<body>"""
 	print """<h3>Hello, """ + str(username) + """.  Please verify your bill below.</h3>"""	
 	print """<table>"""
 	print """<thead> <td>Puppies ordered</td> <td>Number ordered</td> <td>Unit Rental Price</td></thead>"""	
@@ -87,8 +82,6 @@ def generateBill(username, n):
 	<input name = "username" type = "hidden" value =\"""" + str(username) + """\"></input>
 	<input type = "submit" value = "catalogue"></input>
 	</form>
-	</div>
-	</center>
 	</body>
 	</html>"""
 
@@ -97,24 +90,16 @@ This method overwrites the inventory based on the list that contains the entire 
 @param lcpy The list that contains the name, new stock amount, and price of all items.
 """
 def removeInventory(lcpy):
-	with open('../data/Inventory.csv', 'wt') as refile:
-		rewriter = csv.writer(refile, delimiter=',', quotechar=' ', quoting=csv.QUOTE_MINIMAL)
+	with open('../data/Inventory.csv', 'wt') as newinvfile:
 		while len(lcpy) > 0:
-			uname = lcpy.pop()
-			ustock = lcpy.pop()
-			uprice = lcpy.pop()
-			rewriter.writerow([uname,ustock,uprice])
-"""
-This method gets the price of the item from the inventory
-@param pupname The name of the item
-@return Returns the price of the item
-"""
-def getPrice(pupname):
-	with open('../data/Inventory.csv', 'rt') as csfile:
-		freader = csv.reader(csfile, delimiter=',', quotechar=' ')
-		for puppy in freader:
-			if pupname == puppy[0]:
-				return int(puppy[2])
+			uname = str(lcpy.pop())
+			ustock = str(lcpy.pop())
+			uprice = str(lcpy.pop())
+			udescrip = str(lcpy.pop())
+			rewriter = ",".join([uname,ustock,uprice,udescrip])
+			writeline = str(rewriter) + "\n"
+			newinvfile.write(writeline)
+	newinvfile.closed
 
 
 """
@@ -136,34 +121,45 @@ listcopy = []
 3. If the item is not checked off, just add it to the list for inventory update
 """
 with open('../data/Inventory.csv', 'rt') as cofile:
-	coreader = csv.reader(cofile, delimiter=',', quotechar=' ')
-	for p in coreader:
-		checkme = form.getfirst(str(p[0]))
-		if checkme == "true":
-			pupnum = str(p[0]) + 'num'
-			numtoget = form.getfirst(pupnum)
-			if int(p[1]) >= int(numtoget):
-				mygiantlist.append(str(p[0]))
-				mygiantlist.append(numtoget)
-				price = getPrice(str(p[0]))
-				mygiantlist.append(price)
-				listcopy.append(str(p[0]))
-				newstock = int(p[1]) - int(numtoget)
-				listcopy.append(newstock)
-				listcopy.append(price)
-			else:
-				mygiantlist.append(str(p[0]))
-				mygiantlist.append(p[1])
-				price = getPrice(str(p[0]))
-				mygiantlist.append(price)
-				listcopy.append(str(p[0]))
-				listcopy.append(0)
-				listcopy.append(price)
-		else:
+	colist = cofile.read()
+cofile.closed
+splitlist = colist.split("\n")
+splitlist.pop()
+splitlist.reverse()
+print "Content-Type: text/html;charset=utf-8"
+print
+print splitlist
+
+while len(splitlist) > 0:
+	nline = splitlist.pop()
+	p = nline.split(",")
+	checkme = form.getfirst(str(p[0]))
+	if checkme == "true":
+		pupnum = str(p[0]) + 'num'
+		numtoget = form.getfirst(pupnum)
+		if int(p[1]) >= int(numtoget):
+			mygiantlist.append(str(p[0]))
+			mygiantlist.append(numtoget)
+			mygiantlist.append(int(p[2]))
 			listcopy.append(str(p[0]))
-			listcopy.append(int(p[1]))
-			newprice = getPrice(str(p[0]))
-			listcopy.append(newprice)
+			newstock = int(p[1]) - int(numtoget)
+			listcopy.append(newstock)
+			listcopy.append(int(p[2]))
+			listcopy.append(str(p[3]))
+		else:
+			mygiantlist.append(str(p[0]))
+			mygiantlist.append(p[1])
+			mygiantlist.append(int(p[2]))
+			listcopy.append(str(p[0]))
+			listcopy.append(0)
+			listcopy.append(int(p[2]))
+			listcopy.append(str(p[3]))
+	else:
+		listcopy.append(str(p[0]))
+		listcopy.append(int(p[1]))
+		listcopy.append(int(p[2]))
+		listcopy.append(str(p[3]))
+
 """
 Since lists are LIFO in python, reversing the list makes it easier to deal with
 """
@@ -179,11 +175,14 @@ listcopy.reverse()
 """
 if liuser != "null":
 	isLogged = False
-	with open('../data/LoggedIn.csv', 'rt') as logfile:
-		logreader = csv.reader(logfile, delimiter=',', quotechar=' ')
-		for logged in logreader:
-			if liuser == logged[0]:
-				isLogged = True
+	thisline = []
+	with open('../data/LoggedIn.csv', 'rt') as fline:
+		thisline = fline.read()
+	fline.closed
+	mlist = thisline.split("\n")
+	for bits in mlist:
+		if liuser == bits:
+			isLogged = True
 	if isLogged:
 		generateBill(liuser, mygiantlist)
 		removeInventory(listcopy)
