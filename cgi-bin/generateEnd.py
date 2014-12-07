230b726f7ada03c30aa5f76a211f9c53425288d0
Lianna Yang <lianna.yang@mail.mcgill.ca>
2014-12-07 01:45:44 -0500

    added stylesheet to end page

--- cgi-bin/generateEnd.py
+++ cgi-bin/generateEnd.py
@@ -14,19 +14,20 @@ print """
 <!doctype html>
 <html>
 <head>
+<link rel="stylesheet" type="text/css" href="../stylesheets/main.css">
 <title>catalogue</title>
 <style type="text/css"></style>
 </head>
 <body style="color:black; font-family:helvetica;">
-<div style="float:left; width:100%;">
-<div style="text-align:center;
-margin:0 auto;
-width:40%;">
+<div id="home">
+<center>
 <h3>Thank you for your purchase!</h3>
 <p>You can return to the home page by clicking <a href = ../index.html>here</a>.</p>
 <form name="goback" method="post" action="../c.cgi">
 <input name = "username" type = "hidden" value =\"""" + str(liuser) + """\"></input>
 <p>To return to the catalogue, click <input type = "submit" value = "here"></input>.</p>
 </form>
+</center>
+</div>
 </body>
 </html>"""
